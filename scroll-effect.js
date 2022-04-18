9f0c1089cfbe25dd09decbbb5f61660b0d01a974
Robin Singh <robinsingh25oct@gmail.com>
2022-04-18 19:53:34 +0530

    adding files

---
+++ scroll-effect.js
@@ -0,0 +1,21 @@
+// Function used to shrink nav bar removing paddings and adding black background
+$(window).scroll(function() {
+    if ($(document).scrollTop() > 50) {
+        $('.nav').addClass('affix');
+        console.log("OK");
+    } else {
+        $('.nav').removeClass('affix');
+    }
+});
+
+
+
+
+
+
+
+
+
+
+
+
